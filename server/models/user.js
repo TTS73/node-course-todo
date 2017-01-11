46add8b48e47cc678fa1514f2e8ff76282b0e01a
Tero Salomäki <Tero Salomki>
2017-01-11 18:40:49 +0200

    Add POST /todos route and refactor mongoose

---
+++ server/models/user.js
@@ -0,0 +1,12 @@
+let mongoose = require('mongoose');
+
+let User = mongoose.model('Users', {
+  email: {
+    type: String,
+    require: true,
+    trim: true,
+    minlength: 1
+  }
+});
+
+module.exports = {User};
---
+++ server/server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+let bodyParser = require('body-parser');
+
+let {mongoose} = require('./db/mongoose');
+let {Todo} = require('./models/todo');
+let {User} = require('./models/user');
+
+let app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  let todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
+
+// let newTodo = new Todo({
+//   text: 'Cook dinner'
+// });
+//
+// newTodo.save().then((doc) => {
+//   console.log('Saved todo', doc);
+// }, (e) => {
+//   console.log('Unable to save todo');
+// });
+
+// let otherTodo = new Todo({
+//   text: 'Something to do'
+// });
+//
+// otherTodo.save().then((doc) => {
+//   console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//   console.log('Unable to save', e);
+// });
+
+// User model
+// enail - reqiuire it - trim it - set type - set min length to 1
+
+// let newUser = new User({
+//   email: 'joku@jossain.com'
+// });
+//
+// newUser.save().then((doc) => {
+//   console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//   console.log(e);
+// });
