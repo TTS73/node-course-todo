c58697c07518d89dff8fe08c8fc502c645343f7f
Tero Salomäki <Tero Salomki>
2017-01-31 19:47:58 +0200

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const bcrypt = require('bcryptjs');
+
 
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
@@ -115,9 +117,23 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
+app.post('/users/login', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then(user => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
+
 
 module.exports = {app};
 
